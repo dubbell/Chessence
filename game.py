63910bb3691d0c575b299965ddb91311dbae54aa
Linus Lundgren <linus.c.lundgren@gmail.com>
2025-01-10 20:37:00 +0100

    Move calculation.

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ KNIGHT = 4
 BISHOP = 5
 
 
-
+# searching ----------
     
 def locate(sub_board : np.array) -> np.array:
     """Returns list of coordinates for each piece found on the (sub-)board."""
@@ -30,6 +30,20 @@ def locate_first(piece_states : np.array) -> np.array:
             if piece_states[rank, file] > 0:
                 return np.array([rank, file])
 
+def cast_rays(board : np.array, pos : np.array, dirs : np.array, search_condition : Callable[[np.array], bool] = None):
+    """Search from given position by casting rays in the directions given by dirs. search_condition is function
+        that specifies what is being searched for. If not None, then cast_ray returns True when an instance is found.
+        If search_condition is None, then cast_ray returns all pieces that are encountered when searching dirs.
+        
+        Returns True/False if search_condition is not None
+                List of [team, piece_type, rank, file] if search_condition is None"""
+    pass
+
+def is_controlled_by(board : np.array, pos : np.array, team : int):
+    pass
+
+
+# bounds checking -----------
 
 def within_bounds(pos : np.array):
     """True if position is within the bounds of the chess board."""
@@ -40,118 +54,138 @@ def out_of_bounds(pos : np.array):
     return not within_bounds(pos)
     
 
-class Game:
+# other utils -------
 
-    def get_starting_board(self) -> np.array:
-        """Returns board with pieces on starting positions."""
+def get_starting_board() -> np.array:
+    """Returns board with pieces on starting positions."""
 
-        board = np.zeros((12, 8, 8))
-        white_board = board[:6, :, :]
-        black_board = board[6:, :, :]
+    board = np.zeros((12, 8, 8))
+    white_board = board[:6, :, :]
+    black_board = board[6:, :, :]
 
-        white_board[PAWN, 6, :] = 1
-        white_board[KING, 7, 4] = 1
-        white_board[QUEEN, 7, 3] = 1
-        white_board[ROOK, 7, [0, 7]] = 1
-        white_board[KNIGHT, 7, [1, 6]] = 1
-        white_board[BISHOP, 7, [2, 5]] = 1
+    white_board[PAWN, 6, :] = 1
+    white_board[KING, 7, 4] = 1
+    white_board[QUEEN, 7, 3] = 1
+    white_board[ROOK, 7, [0, 7]] = 1
+    white_board[KNIGHT, 7, [1, 6]] = 1
+    white_board[BISHOP, 7, [2, 5]] = 1
+    
+    black_board[PAWN, 1, :] = 1
+    black_board[KING, 0, 4] = 1
+    black_board[QUEEN, 0, 3] = 1
+    black_board[ROOK, 0, [0, 7]] = 1
+    black_board[KNIGHT, 0, [1, 6]] = 1
+    black_board[BISHOP, 0, [2, 5]] = 1
+
+    return board, white_board, black_board
+
+
+# move calculation -------
+
+def get_pawn_moves(team_board : np.array, team_board_pop : np.array, opp_board_pop : np.array, team : int):
+    pawn_moves = []
+
+    pawn_poss = locate(team_board[PAWN])
+    for pawn_pos in pawn_poss:
+        move_dir = 1 if team == BLACK else -1
+
+        # candidate move directions
+        candidate_diffs = np.array([[move_dir, -1], [move_dir, 0], [move_dir, 1]])
+        # candidate destination positions
+        candidate_poss = pawn_pos + candidate_diffs
+
+        to_delete = []
+
+        # disregard first diagonal direction if no opponent at position
+        diag_rank1, diag_file1 = candidate_poss[0]
+        if not opp_board_pop[diag_rank1, diag_file1]:
+            to_delete.append(0)
+
+        # disregard moving forward if position already populated
+        lat_rank, lat_file = candidate_poss[1]
+        if opp_board_pop[lat_rank, lat_file] or team_board_pop[lat_rank, lat_file]:
+            to_delete.append(1)
         
-        black_board[PAWN, 1, :] = 1
-        black_board[KING, 0, 4] = 1
-        black_board[QUEEN, 0, 3] = 1
-        black_board[ROOK, 0, [0, 7]] = 1
-        black_board[KNIGHT, 0, [1, 6]] = 1
-        black_board[BISHOP, 0, [2, 5]] = 1
+        # disregard second diagonal direction if no opponent at position
+        diag_rank2, diag_file2 = candidate_poss[2]
+        if not opp_board_pop[diag_rank2, diag_file2]:
+            to_delete.append(2)
+        
+        # Delete disregarded positions and directions. If empty, move on to next pawn.
+        candidate_diffs = np.delete(candidate_diffs, to_delete, axis=0)
+        candidate_poss = np.delete(candidate_poss, to_delete, axis=0)
+        if len(candidate_poss) == 0:
+            continue
 
-        return board, white_board, black_board
+        # now checking for pins
+
+        # one_dirs is directions from which a pin can exist, lateral and diagonal
+        one_dirs = np.array([[0, -1], [-1, -1], [-1, 0], [-1, 1]])
+        # opposite directions of the ones in one_dirs
+        two_dirs = -one_dirs
+
+        # relevant piece types for corresponding cast
+        relevant_types = [[ROOK, QUEEN] if i % 2 == 0 else [BISHOP, QUEEN] for i in range(4)]
+
+        # results of searching in one_dirs and two_dirs
+        one_casts = cast_rays(pawn_pos, one_dirs)
+        two_casts = cast_rays(pawn_pos, two_dirs)
+        
+        for candidate_pos, candidate_diff in zip(candidate_poss, candidate_diffs):
+            relevant_casts = np.logical_not(
+                np.logical_or(
+                    (candidate_diff == one_dirs).all(axis=1), 
+                    (candidate_diff == two_dirs).all(axis=1)))
+        
+            # true if there's no pin in any direction, in which case the candidate move is added to the results list
+            if not np.all([forward[:2] == [team, KING] and backward[:2] in [[int(not team), t] for t in types]
+                                or backward[:2] == [team, KING] and forward[:2] in [[int(not team), t] for t in types]
+                            for forward, backward, types in zip(one_casts[relevant_casts], two_casts[relevant_casts], relevant_types)]):
+            
+                pawn_moves.append(Move(pawn_pos, candidate_pos, PAWN))
+        
+        return pawn_moves
+
+def get_king_moves(board : np.array, team_board : np.array, team_board_pop : np.array, team : int):
+    king_moves = []
+
+    king_pos = locate_first(team_board[KING])
+    for pos_diff in [np.array([rank, file]) for rank in [0, -1, 1] for file in [0, -1, 1]]:
+        new_pos = new_rank, new_file = king_pos + pos_diff
+        
+        if out_of_bounds(new_pos) or team_board_pop[new_rank, new_file] or is_controlled_by(board, new_pos, not team):
+            continue
+        else:
+            king_moves.append(Move(king_pos, new_pos, KING))
     
+    return king_moves
 
-    def __init__(self):
+def get_queen_moves(board : np.array, team_board : np.array, team : int):
+    queen_moves = []
 
-        self.board, self.white_board, self.black_board = self.get_starting_board()
-
-    
-    def cast_rays(self, pos : np.array, dirs : np.array, search_condition : Callable[[np.array], bool] = None):
-        """Search from given position by casting rays in the directions given by dirs. search_condition is function
-           that specifies what is being searched for. If not None, then cast_ray returns True when an instance is found.
-           If search_condition is None, then cast_ray returns all pieces that are encountered when searching dirs."""
+    queen_poss = locate(team_board[QUEEN])
+    for queen_pos in queen_poss:
         pass
 
 
-    def is_controlled_by(self, board : np.array, pos : np.array, team : int):
-        pass
 
-    
-    def get_moves(self, board : np.array, team : int):
 
-        team_board = board[TEAM_SLICES[team]]
-        opp_board = board[TEAM_SLICES[int(not team)]]
+def get_moves(board : np.array, team : int):
 
-        team_board_populated = team_board.sum(axis=0) > 0
-        opp_board_populated = opp_board.sum(axis=0) > 0
+    team_board = board[TEAM_SLICES[team]]
+    opp_board = board[TEAM_SLICES[int(not team)]]
 
-        available_moves = []  # to be computed
-        
-        # king moves
-        king_pos = locate_first(team_board[KING])
-        for pos_diff in [np.array([rank, file]) for rank in [0, -1, 1] for file in [0, -1, 1]]:
-            new_pos = new_rank, new_file = king_pos + pos_diff
+    team_board_pop = team_board.sum(axis=0) > 0
+    opp_board_pop = opp_board.sum(axis=0) > 0
+
+    available_moves = []  # to be computed
+
+    available_moves.extend(get_pawn_moves(team_board, team_board_pop, opp_board_pop, team))
+    available_moves.extend(get_king_moves(board, team_board, team_board_pop, team))
+
+    return available_moves
             
-            if out_of_bounds(new_pos) or team_board_populated[new_rank, new_file] or self.is_controlled_by(board, new_pos, not team):
-                continue
-            else:
-                available_moves.append(Move(king_pos, new_pos, KING))
-        
-        # pawn moves
-        pawn_poss = locate(team_board[PAWN])
-        for pawn_pos in pawn_poss:
-            move_dir = 1 if team == BLACK else -1
 
-            # candidate move directions
-            candidate_diffs = np.array([[move_dir, -1], [move_dir, 0], [move_dir, 1]])
-            # candidate destination positions
-            candidate_poss = pawn_pos + candidate_diffs
-
-            to_delete = []
-
-            # disregard first diagonal direction if no opponent at position
-            diag_rank1, diag_file1 = candidate_poss[0]
-            if not opp_board_populated[diag_rank1, diag_file1]:
-                to_delete.append(0)
-
-            # disregard moving forward if position already populated
-            lat_rank, lat_file = candidate_poss[1]
-            if opp_board_populated[lat_rank, lat_file] or team_board_populated[lat_rank, lat_file]:
-                to_delete.append(1)
-            
-            # disregard second diagonal direction if no opponent at position
-            diag_rank2, diag_file2 = candidate_poss[2]
-            if not opp_board_populated[diag_rank2, diag_file2]:
-                to_delete.append(2)
-            
-            # Delete disregarded positions and directions. If empty, move on to next pawn.
-            candidate_diffs = np.delete(candidate_diffs, to_delete, axis=0)
-            candidate_poss = np.delete(candidate_poss, to_delete, axis=0)
-            if len(candidate_poss) == 0:
-                continue
-
-            # now checking for pins
-
-            # one_dirs is directions from which a pin can exist, lateral and diagonal
-            one_dirs = np.array([[0, -1], [-1, -1], [-1, 0], [-1, 1]])
-            # opposite directions of the ones in one_dirs
-            two_dirs = -one_dirs
-
-            # results of searching in one_dirs and two_dirs
-            one_casts = self.cast_rays(pawn_pos, one_dirs)
-            two_casts = self.cast_rays(pawn_pos, two_dirs)
-            
-            for candidate_pos, candidate_diff in zip(candidate_poss, candidate_diffs):
-                relevant_casts = np.logical_not(
-                    np.logical_or(
-                        (candidate_diff == one_dirs).all(axis=1), 
-                        (candidate_diff == two_dirs).all(axis=1)))
-            
 
 
 
